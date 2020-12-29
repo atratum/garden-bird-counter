faedf81ef69cfdf107f1ebf9fb0421ee9ff1a987
Jordan Warren <jordanwarren@protonmail.com>
2020-12-29 13:07:42 +0000

    Create styled component: StyledBirdList

--- src/components/BirdList/BirdList.js
+++ src/components/BirdList/BirdList.js
@@ -1,15 +1,30 @@
 import React from 'react';
 
+import styled from 'styled-components'
+
 import BirdItem from '../BirdItem/BirdItem';
 
-function BirdList(props) {
+export default function BirdList(props) {
 
     const {species} = props;
 
-    return species.map((species, key) => {
-        return <BirdItem species={species} key={key} />
-    });
+    return (
+        <StyledBirdList>
+            {species.map((species, key) => {
+                return <BirdItem species={species} key={key} />
+            })}
+        </StyledBirdList>
+    );
     
 }
 
-export default BirdList;
+const StyledBirdList = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    
+    & div {
+        flex-basis: 33.3333%;
+    }
+`;
